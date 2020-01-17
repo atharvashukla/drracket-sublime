917e311910df75afd50791b805ccd10a1175f0d2
Atharva Shukla <33644308+atharvashukla@users.noreply.github.com>
2020-01-16 23:37:28 -0500

    add some colors

---
+++ info.rkt
@@ -0,0 +1,49 @@
+#lang info
+
+#lang info
+
+;; pkg metadata
+(define deps '("base"))
+
+;; colors
+(define backg       #(39 40 34))
+(define foreg       #(248 248 242))
+(define comment     #(117 113 94))
+(define red         #(249 38 115))
+(define orange      #(253 149 31))
+(define lightorange #(230 160 102))
+(define yellow      #(230 219 116))
+(define green       #(166 226 46))
+(define blue        #(102 216 239))
+(define purple      #(173 129 255))
+
+(define framework:color-schemes
+  `(#hash((name . "Monokai")
+          (colors
+           .
+           ((framework:basic-canvas-background                 ,backg)
+            (framework:default-text-color                      ,foreg)
+            (framework:paren-match-color                       ,foreg)
+            (framework:syntax-color:scheme:comment             ,comment)
+            (framework:syntax-color:scheme:constant            ,purple)
+            (framework:syntax-color:scheme:error               ,foreg)
+            (framework:syntax-color:scheme:hash-colon-keyword  ,red)
+            (framework:syntax-color:scheme:keyword             ,foreg)
+            (framework:syntax-color:scheme:string              ,yellow)
+            (framework:syntax-color:scheme:other               ,green)
+            (framework:syntax-color:scheme:parenthesis         ,foreg)
+            (framework:syntax-color:scheme:symbol              ,foreg)
+            (drracket:read-eval-print-loop:error-color         ,foreg)
+            (drracket:read-eval-print-loop:out-color           ,foreg)
+            (drracket:read-eval-print-loop:value-color         ,foreg)
+            (drracket:check-syntax:both-obligation-style-pref  ,foreg)
+            (drracket:check-syntax:free-variable               ,foreg)
+            (drracket:check-syntax:imported                    ,foreg)
+            (drracket:check-syntax:lexically-bound             ,foreg)
+            (drracket:check-syntax:my-obligation-style-pref    ,foreg)
+            (drracket:check-syntax:set!d                       ,foreg)
+            (drracket:check-syntax:their-obligation-style-pref ,foreg)
+            (drracket:check-syntax:unk-obligation-style-pref   ,foreg)
+            (drracket:check-syntax:unused-require              ,foreg))))))
+
+
